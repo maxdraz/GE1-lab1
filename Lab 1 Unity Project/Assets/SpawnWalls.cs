ed62786d5ead36f1aabbe7a867abd2943dadf342
Max <38158634+maxdraz@users.noreply.github.com>
2018-09-23 21:43:56 +0100

    Added colours and 3D for loop

--- Lab 1 Unity Project/Assets/SpawnWalls.cs
+++ Lab 1 Unity Project/Assets/SpawnWalls.cs
@@ -7,26 +7,57 @@ public class SpawnWalls : MonoBehaviour {
     public float wallCount;
     Transform myTransform;
     public float spacing = 5f;
+    public bool is3D;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+
+
 
         myTransform = gameObject.transform;
 
-        for (int x = 0; x < wallCount; x++)
+        if (is3D)
         {
-            for (int y = 0; y< wallCount; y++)
+            for (int x = 0; x < wallCount; x++)
             {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.AddComponent<Rigidbody>();
-                //cube.GetComponent<Rigidbody>().isKinematic = true;
-                //cube.GetComponent<Renderer>().material.color
-                
-                cube.transform.position = new Vector3(myTransform.position.x + x * spacing, myTransform.position.y + y * spacing, myTransform.position.z);
-                //StartCoroutine(SwitchOnKinematic(2f, cube));
+                for (int y = 0; y < wallCount; y++)
+                {
+                    for (int z = 0; z < wallCount; z++)
+                    {
+                        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        cube.AddComponent<Rigidbody>();
+                        //cube.GetComponent<Rigidbody>().isKinematic = true;
+                        cube.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 0.9f, 1f);
+
+                        cube.transform.position = new Vector3(myTransform.position.x + x * spacing, myTransform.position.y + y * spacing, myTransform.position.z + z * spacing);
+                        Debug.Log(cube.transform.position);
+                        //StartCoroutine(SwitchOnKinematic(2f, cube));
+                    }
+                }
             }
         }
-	}
+        else
+        {
+            for (int x = 0; x < wallCount; x++)
+            {
+                for (int y = 0; y < wallCount; y++)
+                {
+                    
+                  
+                        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        cube.AddComponent<Rigidbody>();
+                        //cube.GetComponent<Rigidbody>().isKinematic = true;
+                        cube.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 0.9f, 1f);
+
+                        cube.transform.position = new Vector3(myTransform.position.x + x * spacing, myTransform.position.y + y * spacing, myTransform.position.z);
+                        Debug.Log(cube.transform.position); 
+                        //StartCoroutine(SwitchOnKinematic(2f, cube));
+                    
+                }
+            }
+        }
+    }
 	
    // IEnumerator SwitchOnKinematic(float time, GameObject cube)
    // {
